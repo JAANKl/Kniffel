b26c42b360b39da701f86be287a6f5186c0b667a
JAANKl <89450417+JAANKl@users.noreply.github.com>
2021-08-24 15:20:03 +0200

    Add files via upload

--- Hello_World.py
+++ Hello_World.py
@@ -2,3 +2,17 @@
 #24.08.21
 #test Hello World
 print("Hello World")
+
+import random
+
+class Game:
+	__init__(self):
+		registered = {}
+
+class Dice:
+	__init__(self):
+		self.role = []
+		self.numberOf = {}
+		self.sumOf = {}
+		self.sum = 0
+		
