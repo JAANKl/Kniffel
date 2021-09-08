5ddce477f2f1b764771caec38550455888e23775
JAANKl <89450417+JAANKl@users.noreply.github.com>
2021-09-08 12:51:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import sys
-from PyQt5 import uic, QtWidgets
+from PyQt5 import uic, QtWidgets, QtCore
 
 Ui_MainWindow, WindowBaseClass = uic.loadUiType("main_window.ui")
 class MyDialog(WindowBaseClass, Ui_MainWindow):
+    chosenDice = QtCore.pyqtSignal(str) # die Würfel die man behalten will (Übergabe an Label)
+
     def __init__(self, parent=None):
         WindowBaseClass.__init__(self, parent)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+
+    def checkChosenDice(self):
+        chosen = []
+        if self.checkBoxWuerfel1.isChecked():
+            chosen.append(1)
+        if self.checkBoxWuerfel2.isChecked():
+            chosen.append(2)
+        if self.checkBoxWuerfel3.isChecked():
+            chosen.append(3)
+        if self.checkBoxWuerfel4.isChecked():
+            chosen.append(4)
+        if self.checkBoxWuerfel5.isChecked():
+            chosen.append(5)
+        print(chosen)
+        chosen = ",".join([str(zahl) for zahl in chosen])
+        self.chosenDice.emit(chosen)
+
 
 if __name__ == "__main__":
     application = QtWidgets.QApplication(sys.argv)
