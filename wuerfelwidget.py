0cae1cf1a36c076f744b8551fe914215945f03f0
kru00 <89452612+kru00@users.noreply.github.com>
2021-09-08 12:52:51 +0200

    Update wuerfelwidget.py

--- wuerfelwidget.py
+++ wuerfelwidget.py
@@ -3,10 +3,19 @@ import os
 from PyQt5.QtGui import QPainter, QPixmap
 from PyQt5.QtWidgets import QWidget
 from PyQt5.Qt import Qt
+from PyQt5 import QtCore
 
 from kniffel_gui import Game
 
 class WuerfelWidget(QWidget):
+
+    showPossibilities = QtCore.pyqtSignal(str)
+    showRegistered = QtCore.pyqtSignal(str)
+    showPlayerName = QtCore.pyqtSignal(str)
+    showRoundCounter = QtCore.pyqtSignal(str)
+    showRollCounter = QtCore.pyqtSignal(str)
+
+
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.dice_size = 50
@@ -33,9 +42,110 @@ class WuerfelWidget(QWidget):
 
     def wuerfeln(self):
         self._logic.rollDice([0, 1, 2, 3, 4])
+        self._logic.playerRollCounter += 1
+        self.showPlayerName.emit(self._logic.player.name)
+        self.showRoundCounter.emit(str(1))
+        self.showRollCounter.emit(str(self._logic.playerRollCounter))
+
+        possibilities = self._logic.possibilities()
+        text = ""
+        for name in possibilities.table:
+            text += name + ": " + str(possibilities.table[name]) + "\n"
+        self.showPossibilities.emit(text)
         self.update()
 
-    def eintragen(self, name):
+    def registerEinser(self):
+        self._logic.register("Einser")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerZweier(self):
+        self._logic.register("Zweier")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerDreier(self):
+        self._logic.register("Dreier")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerVierer(self):
+        self._logic.register("Vierer")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerFuenfer(self):
+        self._logic.register("Fünfer")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerSechser(self):
+        self._logic.register("Sechser")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerDreierpasch(self):
+        self._logic.register("Dreierpasch")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerViererpasch(self):
+        self._logic.register("Viererpasch")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerFullHouse(self):
+        self._logic.register("FullHouse")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerKleineStrasse(self):
+        self._logic.register("KleineStrasse")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerGrosseStrasse(self):
+        self._logic.register("GrosseStrasse")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerKniffel(self):
+        self._logic.register("Kniffel")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+    
+    def registerChance(self):
+        self._logic.register("Chance")
+        text = ""
+        for figur in self._logic.player.registered.table:
+            text += figur + ": " + str(self._logic.player.registered.table[figur]) + "\n"
+        self.showRegistered.emit(text)
+
+    def possibilities(self):
         pass
 
     def paintEvent(self, event):
