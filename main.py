9ccfb94a00776c54a8ddcbbfb57f21ccade40e20
kru00 <89452612+kru00@users.noreply.github.com>
2021-09-13 12:57:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ from PyQt5 import uic, QtWidgets, QtCore
 
 Ui_MainWindow, WindowBaseClass = uic.loadUiType("main_window.ui")
 class MyDialog(WindowBaseClass, Ui_MainWindow):
-    chosenDice = QtCore.pyqtSignal(str) # die Würfel die man behalten will (Übergabe an Label)
 
     def __init__(self, parent=None):
         WindowBaseClass.__init__(self, parent)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
-    def checkChosenDice(self):
+        self.startedGame = False
+
+    def acceptPlayerName(self):
+        if not self.startedGame:
+            name = self.textEdit_PlayerName.toPlainText()
+            self.widget._logic.playerNames.append(name)
+            self.widget._logic.numberOfPlayers = len(self.widget._logic.playerNames)
+            print(self.widget._logic.playerNames)
+    
+    def startGame(self):
+        self.startedGame = True
+
+    def wuerfeln(self):
         chosen = []
         if not self.checkBoxWuerfel1.isChecked():
             chosen.append(0)
@@ -22,11 +33,50 @@ class MyDialog(WindowBaseClass, Ui_MainWindow):
             chosen.append(3)
         if not self.checkBoxWuerfel5.isChecked():
             chosen.append(4)
-        #self.widget._logic.rollDice(chosen, self.widget._logic.playerRollCounter)
-        #self.widget._logic.playerRollCounter += 1
-        print(chosen)
-        chosen = ",".join([str(zahl) for zahl in chosen])
-        self.chosenDice.emit(chosen)
+
+        if self.widget._logic.playerRollCounter == 0:
+            self.widget.wuerfeln([0, 1, 2, 3, 4])
+        else:
+            self.widget.wuerfeln(chosen)
+    
+    def registerEinser(self):
+        self.widget.register("Einser")
+
+    def registerZweier(self):
+        self.widget.register("Zweier")
+
+    def registerDreier(self):
+        self.widget.register("Dreier")
+
+    def registerVierer(self):
+        self.widget.register("Vierer")
+
+    def registerFuenfer(self):
+        self.widget.register("Fünfer")
+
+    def registerSechser(self):
+        self.widget.register("Sechser")
+
+    def registerDreierpasch(self):
+        self.widget.register("Dreierpasch")
+
+    def registerViererpasch(self):
+        self.widget.register("Viererpasch")
+
+    def registerFullHouse(self):
+        self.widget.register("FullHouse")
+
+    def registerKleineStrasse(self):
+        self.widget.register("KleineStrasse")
+
+    def registerGrosseStrasse(self):
+        self.widget.register("GrosseStrasse")
+
+    def registerKniffel(self):
+        self.widget.register("Kniffel")
+    
+    def registerChance(self):
+        self.widget.register("Chance")
 
 
 if __name__ == "__main__":
